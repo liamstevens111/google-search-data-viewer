85f1e9f4ab549ad0f4652f4dff9b97e486a9c828
Liam Stevens <liamstevens111@gmail.com>
2022-05-10 16:02:17 +0700

    [#3] Add tests for ensure_authenticated plug

---
+++ test/google_search_data_viewer_web/plugs/ensure_authenticated_test.exs
@@ -0,0 +1,26 @@
+defmodule GoogleSearchDataViewerWeb.EnsureAuthenticatedPlugTest do
+  use GoogleSearchDataViewerWeb.ConnCase, async: true
+
+  alias GoogleSearchDataViewerWeb.EnsureAuthenticatedPlug
+
+  describe "init/1" do
+    test "returns given options" do
+      assert EnsureAuthenticatedPlug.init([]) == []
+    end
+  end
+
+  describe "call/2" do
+    test "given an unauthenticated user, renders home page", %{conn: conn} do
+      conn =
+        conn
+        |> init_test_session(%{})
+        |> fetch_flash()
+        |> EnsureAuthenticatedPlug.call([])
+
+      assert conn.assigns.current_user == nil
+      assert get_flash(conn, :error) =~ "Please sign in to use this service"
+      assert redirected_to(conn, 302) =~ "/"
+      assert conn.halted == true
+    end
+  end
+end
