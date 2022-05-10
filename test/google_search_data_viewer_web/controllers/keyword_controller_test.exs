b6799cf2be8d0714b5621f625e775ea4c987457f
Liam Stevens <liamstevens111@gmail.com>
2022-05-10 16:01:53 +0700

    [#3] Add tests for keyword controller and test csv file

--- test/google_search_data_viewer_web/controllers/keyword_controller_test.exs
+++ test/google_search_data_viewer_web/controllers/keyword_controller_test.exs
@@ -1,5 +1,35 @@
 defmodule GoogleSearchDataViewerWeb.KeywordControllerTest do
-  use GoogleSearchDataViewerWeb.ConnCase
+  use GoogleSearchDataViewerWeb.ConnCase, async: true
 
+  describe "GET /keywords" do
+    test "renders keywords page", %{conn: conn} do
+      user = insert(:user)
 
+      conn =
+        conn
+        |> init_test_session(user_id: user.id)
+        |> get(Routes.keyword_path(conn, :index))
+
+      assert html_response(conn, 200) =~ "Select a csv file for upload"
+    end
+  end
+
+  describe "POST /keywords/upload" do
+    test "given a valid csv file extention, uploads the file", %{conn: conn} do
+      file = %Plug.Upload{
+        path: "test/support/fixtures/keywords/keywords.csv",
+        filename: "keywords.csv"
+      }
+
+      user = insert(:user)
+
+      conn =
+        conn
+        |> init_test_session(user_id: user.id)
+        |> post("/keywords/upload", %{:file => file})
+
+      assert redirected_to(conn, 302) =~ "/keywords"
+      assert get_flash(conn, :info) =~ "File successfully uploaded"
+    end
+  end
 end
