5a3cb334ad531b99f6e5fd1b9848967cbe171cf0
Liam Stevens <liamstevens111@gmail.com>
2022-05-11 14:31:59 +0700

    [#18] Add Keyword Controller and helper function for validate/parse csv

--- lib/google_search_data_viewer_web/controllers/keyword_controller.ex
+++ lib/google_search_data_viewer_web/controllers/keyword_controller.ex
@@ -1,11 +1,28 @@
 defmodule GoogleSearchDataViewerWeb.KeywordController do
   use GoogleSearchDataViewerWeb, :controller
 
+  alias GoogleSearchDataViewerWeb.KeywordHelper
+
   def index(conn, _params), do: render(conn, "index.html")
 
-  def upload(conn, %{"file" => _file}) do
-    conn
-    |> put_flash(:info, "File successfully uploaded")
-    |> redirect(to: Routes.keyword_path(conn, :index))
+  def upload(conn, %{"file" => file}) do
+    case KeywordHelper.validate_and_parse_keyword_file(file) do
+      {:ok, _keywords} ->
+        conn
+        |> put_flash(:info, "File successfully uploaded")
+        |> redirect(to: Routes.keyword_path(conn, :index))
+
+      {:error, :invalid_format} ->
+        conn
+        |> put_flash(:error, "Format invalid")
+        |> redirect(to: Routes.keyword_path(conn, :index))
+        |> halt()
+
+      {:error, :invalid_length} ->
+        conn
+        |> put_flash(:error, "Length invalid")
+        |> redirect(to: Routes.keyword_path(conn, :index))
+        |> halt()
+    end
   end
 end
