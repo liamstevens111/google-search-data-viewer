f3154b7798a95962195f25541ebfbeec3220a043
Liam Stevens <liamstevens111@gmail.com>
2022-05-17 10:33:08 +0700

    #[1] Write tests for fetching KeywordUploads for a particular user

--- test/google_search_data_viewer/keywords/keyword_test.exs
+++ test/google_search_data_viewer/keywords/keyword_test.exs
@@ -4,11 +4,36 @@ defmodule GoogleSearchDataViewer.Keywords.KeywordTest do
   alias GoogleSearchDataViewer.Keywords.Keyword
 
   describe "create_keyword_uploads/2" do
-    test "give a valid list of keywords and a user, creates keywords for the user" do
+    test "given a valid list of keywords and a user, creates keywords for the user" do
       user = insert(:user)
       keywords = ["dog", "cat", "fish"]
 
-      assert Keyword.create_keyword_uploads(keywords, user) == Enum.count(keywords)
+      {keyword_count, _keywords} = Keyword.create_keyword_uploads(keywords, user)
+
+      assert keyword_count == Enum.count(keywords)
+    end
+  end
+
+  describe "get_keyword_uploads_for_user/1" do
+    test "given an existing user with uploaded keywords, lists keywords for the user" do
+      user = insert(:user)
+      keywords = ["dog", "cat", "fish"]
+
+      {_keyword_count, returned_keywords} = Keyword.create_keyword_uploads(keywords, user)
+
+      assert Enum.map(returned_keywords, & &1.name) == keywords
+      assert Keyword.get_keyword_uploads_for_user(user) == returned_keywords
+    end
+
+    test "given an existing user with no uploaded keywords, returns no keywords for the user" do
+      user1 = insert(:user)
+      user2 = insert(:user)
+
+      keywords = ["dog", "cat", "fish"]
+
+      Keyword.create_keyword_uploads(keywords, user1)
+
+      assert Keyword.get_keyword_uploads_for_user(user2) == []
     end
   end
 end
