421d71fb2c5b9e1cc51a96b8aa2f48dc1e391b30
Liam Stevens <liamstevens111@gmail.com>
2022-05-23 14:14:34 +0700

    [#24] Fix oban job not scheduling with delay

--- lib/google_search_data_viewer_worker/keywords/helpers/job_creation.ex
+++ lib/google_search_data_viewer_worker/keywords/helpers/job_creation.ex
@@ -2,13 +2,16 @@ defmodule GoogleSearchDataViewerWorker.Keywords.JobCreationHelper do
   alias GoogleSearchDataViewerWorker.Keywords.SearchWorker
 
   def create_keyword_upload_jobs_with_delay(keyword_uploads) do
+    a =
     keyword_uploads
     |> Enum.zip_with(
-      Enum.map(Enum.to_list(0..Enum.count(keyword_uploads)), &(&1 * 3)),
-      fn keyword_upload, interval ->
-        SearchWorker.new(%{keyword_id: keyword_upload.id, interval_in: interval})
+      Enum.map(Enum.to_list(0..(Enum.count(keyword_uploads) - 1)), &(&1 * 3)),
+      fn keyword_upload, delay ->
+        SearchWorker.new(%{keyword_id: keyword_upload.id}, schedule_in: delay)
       end
     )
     |> Oban.insert_all()
+
+    IO.inspect(a)
   end
 end
