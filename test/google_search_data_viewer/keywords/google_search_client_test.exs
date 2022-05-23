03ab573bc48cf04922660dc40c419b29b5b037cd
Liam Stevens <liamstevens111@gmail.com>
2022-05-23 15:51:53 +0700

    [#24] Add test using ExVCR to ensure GoogleSearchClient returns valid response

---
+++ test/google_search_data_viewer/keywords/google_search_client_test.exs
@@ -0,0 +1,18 @@
+defmodule GoogleSearchDataViewer.Keywords.GoogleSearchClientTest do
+  use ExUnit.Case, async: false
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
+  alias GoogleSearchDataViewer.Keywords.GoogleSearchClient
+
+  setup_all do
+    HTTPoison.start
+  end
+
+  describe "get_html/1" do
+    test "given a keyword, returns ok and body" do
+      use_cassette "keywords/search_dog" do
+        assert {:ok, _html_response} = GoogleSearchClient.get_html("dog")
+      end
+    end
+  end
+end
