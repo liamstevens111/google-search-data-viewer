8e036a3e9112bf356574172b6e8e6cba34f4ab36
Liam Stevens <liamstevens111@gmail.com>
2022-05-24 13:27:30 +0700

    [#24] Change from Enum.zip_with function to Enum.with_index to generate delays for each job to improve readability

--- lib/google_search_data_viewer_worker/keywords/helpers/job_creation.ex
+++ lib/google_search_data_viewer_worker/keywords/helpers/job_creation.ex
@@ -3,12 +3,10 @@ defmodule GoogleSearchDataViewerWorker.Keywords.JobCreationHelper do
 
   def create_keyword_upload_jobs_with_delay(keyword_uploads, delay \\ 3) do
     keyword_uploads
-    |> Enum.zip_with(
-      Enum.map(Enum.to_list(0..(Enum.count(keyword_uploads) - 1)), &(&1 * delay)),
-      fn keyword_upload, delay ->
-        SearchWorker.new(%{keyword_id: keyword_upload.id}, schedule_in: delay)
-      end
-    )
+    |> Enum.with_index()
+    |> Enum.map(fn {keyword_upload, index} ->
+      SearchWorker.new(%{keyword_id: keyword_upload.id}, schedule_in: index * delay)
+    end)
     |> Oban.insert_all()
   end
 end
