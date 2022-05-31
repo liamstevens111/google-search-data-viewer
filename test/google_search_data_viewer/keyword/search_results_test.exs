db85b66ae557419d7bdbd562f883f61c09826ccb
Liam Stevens <liamstevens111@gmail.com>
2022-05-31 15:04:28 +0700

    [#24] Add additional tests to ensure errors are created for invalid search url data

--- test/google_search_data_viewer/keyword/search_results_test.exs
+++ test/google_search_data_viewer/keyword/search_results_test.exs
@@ -21,5 +21,37 @@ defmodule GoogleSearchDataViewer.Keyword.SearchResultsTest do
       assert search_result.is_adword == true
       assert search_result.keyword_upload_id == keyword_upload.id
     end
+
+    test "given an empty url, returns an error" do
+      keyword_upload = insert(:keyword_upload)
+
+      search_result_url_data = %{
+        url: "",
+        is_top_adword: false,
+        is_adword: true,
+        keyword_upload_id: keyword_upload.id
+      }
+
+      {:error, changeset} = SearchResults.create_search_results(search_result_url_data)
+
+      assert errors_on(changeset) == %{
+               url: ["can't be blank"]
+             }
+    end
+
+    test "given an invalid keyword upload, returns an error" do
+      search_result_url_data = %{
+        url: "www.google.com",
+        is_top_adword: false,
+        is_adword: true,
+        keyword_upload_id: -1
+      }
+
+      {:error, changeset} = SearchResults.create_search_results(search_result_url_data)
+
+      assert errors_on(changeset) == %{
+               keyword_upload: ["does not exist"]
+             }
+    end
   end
 end
