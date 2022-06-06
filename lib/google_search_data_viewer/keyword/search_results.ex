4d6f7aaae4cac00c6a701de6191f3ae559017d53
Liam Stevens <liamstevens111@gmail.com>
2022-06-06 10:06:03 +0700

    [#19] Initial queries to get keyword list and summary data

--- lib/google_search_data_viewer/keyword/search_results.ex
+++ lib/google_search_data_viewer/keyword/search_results.ex
@@ -1,10 +1,18 @@
 defmodule GoogleSearchDataViewer.Keyword.SearchResults do
   alias GoogleSearchDataViewer.Keyword.Schemas.SearchResultUrl
+  alias GoogleSearchDataViewer.Keyword.Queries.SearchResultsQuery
   alias GoogleSearchDataViewer.Repo
 
   def create_search_results(url_stats) do
     url_stats
     |> SearchResultUrl.changeset()
     |> Repo.insert()
+  end
+
+  def get_report_for_keyword_upload(keyword_upload_id) do
+    keyword_upload_urls =
+      keyword_upload_id
+      |> SearchResultsQuery.get_search_results_for_keyword_upload()
+      |> SearchResultsQuery.get_search_results_summary_data()
   end
 end
