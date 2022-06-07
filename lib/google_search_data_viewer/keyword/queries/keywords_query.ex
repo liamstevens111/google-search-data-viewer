c8a112703295e7d923f9655ac481d62ff95e0bf1
Liam Stevens <liamstevens111@gmail.com>
2022-06-07 11:47:48 +0700

    [#19] Remove function that performs DB query to group by url type

--- lib/google_search_data_viewer/keyword/queries/keywords_query.ex
+++ lib/google_search_data_viewer/keyword/queries/keywords_query.ex
@@ -10,21 +10,9 @@ defmodule GoogleSearchDataViewer.Keyword.Queries.KeywordsQuery do
     |> select([:id, :user_id, :name, :status, :updated_at, :inserted_at])
   end
 
-  def list_for_keyword_with_search_results(keyword_upload_id) do
+  def get_keyword_upload_with_search_results(keyword_upload_id) do
     KeywordUpload
-    |> join(:left, [keyword], url in assoc(keyword, :search_result_urls))
     |> preload(:search_result_urls)
     |> where([keyword], keyword.id == ^keyword_upload_id)
-  end
-
-  def url_stats_by_search_results(query) do
-    query
-    |> exclude(:preload)
-    |> group_by([_, url], fragment("GROUPING SETS ((), (?, ?))", url.is_adword, url.is_top_adword))
-    |> select([_, url], %{
-      is_adword: url.is_adword,
-      is_top_adword: url.is_top_adword,
-      count: count()
-    })
   end
 end
