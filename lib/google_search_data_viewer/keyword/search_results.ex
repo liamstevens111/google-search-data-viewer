433d88fd613c5647e46f232ea7324bf2267f05bf
Liam Stevens <liamstevens111@gmail.com>
2022-06-08 12:58:16 +0700

    [#19] Refactor function to handle grouping of URL data based upon adword type

--- lib/google_search_data_viewer/keyword/search_results.ex
+++ lib/google_search_data_viewer/keyword/search_results.ex
@@ -9,18 +9,10 @@ defmodule GoogleSearchDataViewer.Keyword.SearchResults do
   end
 
   def group_by_adword_types(search_result_urls) do
-    search_result_urls
-    |> Enum.group_by(&{&1.is_adword, &1.is_top_adword})
-    |> Enum.map(&add_adword_type_from_group/1)
-    |> Map.new()
+    Enum.group_by(search_result_urls, &group_adword_type/1)
   end
 
-  defp add_adword_type_from_group({{true, true}, search_result_urls}),
-    do: {:top_adwords, search_result_urls}
-
-  defp add_adword_type_from_group({{false, false}, search_result_urls}),
-    do: {:non_adwords, search_result_urls}
-
-  defp add_adword_type_from_group({{true, false}, search_result_urls}),
-    do: {:adwords, search_result_urls}
+  defp group_adword_type(%SearchResultUrl{is_adword: true, is_top_adword: true}), do: :top_adwords
+  defp group_adword_type(%SearchResultUrl{is_adword: false, is_top_adword: false}), do: :non_adwords
+  defp group_adword_type(%SearchResultUrl{is_adword: true, is_top_adword: false}), do: :adwords
 end
