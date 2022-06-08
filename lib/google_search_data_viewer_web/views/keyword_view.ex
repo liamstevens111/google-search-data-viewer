b5d3b10d12d74a07c1f479138f33fb8937858483
Liam Stevens <liamstevens111@gmail.com>
2022-06-08 10:28:48 +0700

    [#2] Add page for displaying URL results for the KeywordUpload

--- lib/google_search_data_viewer_web/views/keyword_view.ex
+++ lib/google_search_data_viewer_web/views/keyword_view.ex
@@ -4,4 +4,19 @@ defmodule GoogleSearchDataViewerWeb.KeywordView do
   def format_date_time(datetime) do
     Calendar.strftime(datetime, "%d.%m.%y %H:%M:%S")
   end
+
+  def total_urls(search_results) do
+    search_results
+    |> Enum.map(fn {_, search_result_urls} -> Enum.count(search_result_urls) end)
+    |> Enum.sum()
+  end
+
+  def total_top_adword_urls(search_results),
+    do: Enum.count(Map.get(search_results, :top_adwords, []))
+
+  def total_adword_urls(search_results),
+    do: Enum.count(Map.get(search_results, :adwords, []))
+
+  def total_non_adword_urls(search_results),
+    do: Enum.count(Map.get(search_results, :non_adwords, []))
 end
