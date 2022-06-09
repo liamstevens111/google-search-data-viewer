dda622557e2dcf473fa57f76ad2de028b225c9e3
Liam Stevens <liamstevens111@gmail.com>
2022-06-09 12:03:11 +0700

    [#2] Refactor router to use resource instead of individual endpoints

--- lib/google_search_data_viewer_web/router.ex
+++ lib/google_search_data_viewer_web/router.ex
@@ -39,8 +39,7 @@ defmodule GoogleSearchDataViewerWeb.Router do
   scope "/keywords", GoogleSearchDataViewerWeb do
     pipe_through [:browser, :authorized]
 
-    get "/", KeywordController, :index
-    get "/:id", KeywordController, :show
+    resources "/", KeywordController, only: [:index, :show]
 
     post "/upload", KeywordController, :upload
   end
