51846fdda0d0e96411e2bf8a5f3f00e1047935d8
Liam Stevens <liamstevens111@gmail.com>
2022-06-09 14:08:48 +0700

    [#2] Group related test functions together in same file

--- test/google_search_data_viewer_web/views/keyword_view_test.exs
+++ test/google_search_data_viewer_web/views/keyword_view_test.exs
@@ -3,6 +3,14 @@ defmodule GoogleSearchDataViewerWeb.KeywordViewTest do
 
   alias GoogleSearchDataViewerWeb.KeywordView
 
+  describe "format_date_time/1" do
+    test "given a date and time, returns it in the format of d.m.y H:M:S" do
+      {_, current_date_time} = NaiveDateTime.new(1970, 1, 1, 0, 0, 0)
+
+      assert KeywordView.format_date_time(current_date_time) == "01.01.70 00:00:00"
+    end
+  end
+
   describe "total_urls/1" do
     test "given a list of search results containing three URLs, correctly returns the count" do
       search_results = %{
